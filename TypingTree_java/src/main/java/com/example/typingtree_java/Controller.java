d373492940f74b4e5384220a5318003388a185b9
Stefano Manescotto <stefano.manescotto@edu.unito.it>
2022-07-11 22:13:00 +0200

    Added auto scroll for textArea

--- TypingTree_java/src/main/java/com/example/typingtree_java/Controller.java
+++ TypingTree_java/src/main/java/com/example/typingtree_java/Controller.java
@@ -2,18 +2,9 @@ package com.example.typingtree_java;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
 import org.fxmisc.richtext.*;
 
@@ -27,7 +18,7 @@ public class Controller implements Initializable {
     @FXML
     private InlineCssTextArea txtArea;
     private boolean error = false;
-    private int lastError = Integer.MAX_VALUE, currentWord = 0, lengthWrote = 0, lastLine = 0, fontSize = 30;
+    private int lastError = Integer.MAX_VALUE, currentWord = 0, lengthWrote = 0, lastLine = 0, fontSize = 20;
 
     public void initialize(URL location, ResourceBundle resources) {
         txtArea.setEditable(false);
@@ -52,21 +43,34 @@ public class Controller implements Initializable {
         String toWrite;
         toWrite = txtArea.getText().substring(currentWord);
 
-        System.out.println(txtArea.lineIndex(0, currentWord));
+
         //txtArea.line;
 
         //txtArea.prevLine(NavigationActions.SelectionPolicy.ADJUST);
-        txtArea.getCurrentLineStartInParargraph();
+       // txtArea.getCurrentLineStartInParargraph();
         //txtArea.showParagraphAtCenter(currentWord);
-       // txtArea.scrollYToPixel((fontSize + fontSize/2) * txtArea.lineIndex(0, currentWord + txtField.getText().length()));
+
+        txtArea.scrollYToPixel((txtArea.lookup(".text").getBoundsInLocal().getHeight()/txtArea.getParagraphLinesCount(0))
+                * txtArea.lineIndex(0, currentWord + txtField.getText().length()));
 
        /* if(lastLine != txtArea.lineIndex(0, currentWord)){
             //txtArea.scrollYBy(10 * txtArea.lineIndex(0, currentWord));
             txtArea.scrollYToPixel(30 * txtArea.lineIndex(0, currentWord));
             lastLine = txtArea.lineIndex(0, currentWord);
         }*/
-        txtArea.wrapTextProperty().set(true);
+        //txtArea.wrapTextProperty().set(true);
         if(txtField.getText().length() > 0){
+            //System.out.println(txtArea.lineIndex(0, currentWord) + " " + new Text(txtArea.getText()).getLayoutBounds().getHeight());
+
+            /*Text t = new Text(txtArea.getText());
+            t.setStyle("-fx-font-size: " + fontSize);
+
+            Font f = new Font(fontSize);
+
+            System.out.println(txtArea.lineIndex(0, currentWord) + " - " + txtArea.lookup(".text").
+                    getBoundsInLocal().getHeight()/txtArea.getParagraphLinesCount(0));*/
+
+
             if(txtField.getText().equals(toWrite.substring(0, txtField.getText().length()))){
                 Border b = new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
                 txtField.setBorder(b);
@@ -84,16 +88,31 @@ public class Controller implements Initializable {
                     txtField.setText("");
                     //txtField.getAlignment()
                 }else{
-                    Text t = new Text();
+                    /*Text t = new Text();
                     t.setStyle("-fx-fill: #4F8A10;-fx-font-weight:bold;");
-                    t.setText(txtArea.getText());
+                    t.setText(txtArea.getText());*/
                 }
             }else{
                 Border b = new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
                 txtField.setBorder(b);
                 //txtArea.setStyle(1, 2, "");
+                //txtArea.getCaretBounds().get().getHeight();
 
             }
         }
     }
+
+   /* private double computeTextWidth(Font font, String text, double wrappingWidth) {
+        Text helper = new Text();
+        helper.setFont(font);
+        helper.setText(text);
+        // Note that the wrapping width needs to be set to zero before
+        // getting the text's real preferred width.
+        helper.setWrappingWidth(0);
+        helper.setLineSpacing(0);
+        double w = Math.min(helper.prefHeight(-1), wrappingWidth);
+        helper.setWrappingWidth((int)Math.ceil(w));
+        double textWidth = Math.ceil(helper.getLayoutBounds().getHeight());
+        return textWidth;
+    }*/
 }
