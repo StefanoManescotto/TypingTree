dbbe5335f41881dd72b61e06c06317578fe0800f
Stefano Manescotto <stefano.manescotto@edu.unito.it>
2022-07-13 22:27:13 +0200

    Added basic text color based on right/wrong typing

--- TypingTree_java/src/main/java/com/example/typingtree_java/Controller.java
+++ TypingTree_java/src/main/java/com/example/typingtree_java/Controller.java
@@ -2,6 +2,7 @@ package com.example.typingtree_java;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -9,6 +10,7 @@ import javafx.scene.paint.Color;
 import org.fxmisc.richtext.*;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -19,6 +21,7 @@ public class Controller implements Initializable {
     private InlineCssTextArea txtArea;
     private boolean error = false;
     private int lastError = Integer.MAX_VALUE, currentWord = 0, lengthWrote = 0, lastLine = 0, fontSize = 20;
+    private double txtHeight = 0;
 
     public void initialize(URL location, ResourceBundle resources) {
         txtArea.setEditable(false);
@@ -30,8 +33,17 @@ public class Controller implements Initializable {
         txtArea.setWrapText(true);
         //txtArea.setConte;
 
-        txtArea.setStyle("-fx-font-size: " + fontSize);
+        txtArea.setStyle("-fx-font-size: " + fontSize+";" +"-fx-text-fill: red;");
+       // txtArea.clearStyle(0,5);
+        //txtArea.setStyle(0, 6, "-fx-stroke: red;");
+        //txtArea.setStyle(0, 10, "-fx-stroke: red;");
+        //txtArea.setStyle(30, 50, "-fx-font-weight: bold;");
+        //txtArea.setStyle("-fx-font-size: " + fontSize+";" +"-fx-text-fill: red;");
+        //System.out.println("3: " + txtArea.getStyle());
+
         txtArea.setShowCaret(Caret.CaretVisibility.ON);
+
+        //txtArea.setStyle("-fx-text-fill: red");
     }
     @FXML
     protected void testingButton(){
@@ -43,15 +55,31 @@ public class Controller implements Initializable {
         String toWrite;
         toWrite = txtArea.getText().substring(currentWord);
 
+        txtHeight = 0;
+        for (Node n: txtArea.lookupAll(".text")) {
+            if(txtHeight < n.boundsInParentProperty().get().getMaxY()){
+                txtHeight = n.boundsInParentProperty().get().getMaxY();
+            }
+        }
 
+        //((List)(txtArea.lookupAll(".text"))).get(txtArea.lookupAll(".text").size() - 1);
+
+        //txtArea.look
+        //txtArea.lookupAll("").
         //txtArea.line;
 
         //txtArea.prevLine(NavigationActions.SelectionPolicy.ADJUST);
        // txtArea.getCurrentLineStartInParargraph();
         //txtArea.showParagraphAtCenter(currentWord);
 
-        txtArea.scrollYToPixel((txtArea.lookup(".text").getBoundsInLocal().getHeight()/txtArea.getParagraphLinesCount(0))
+        /*txtArea.scrollYToPixel((txtHeight/txtArea.getParagraphLinesCount(0))
+                * txtArea.lineIndex(0, currentWord + txtField.getText().length()));*/
+
+        txtArea.scrollYToPixel((txtHeight/txtArea.getParagraphLinesCount(0))
                 * txtArea.lineIndex(0, currentWord + txtField.getText().length()));
+
+        System.out.println(" -- " + txtField.getText().length() + " -- " + txtArea.getParagraphLinesCount(0) + " -- " +
+                txtArea.lineIndex(0, currentWord + txtField.getText().length()));
 
        /* if(lastLine != txtArea.lineIndex(0, currentWord)){
             //txtArea.scrollYBy(10 * txtArea.lineIndex(0, currentWord));
@@ -59,6 +87,7 @@ public class Controller implements Initializable {
             lastLine = txtArea.lineIndex(0, currentWord);
         }*/
         //txtArea.wrapTextProperty().set(true);
+
         if(txtField.getText().length() > 0){
             //System.out.println(txtArea.lineIndex(0, currentWord) + " " + new Text(txtArea.getText()).getLayoutBounds().getHeight());
 
@@ -70,13 +99,13 @@ public class Controller implements Initializable {
             System.out.println(txtArea.lineIndex(0, currentWord) + " - " + txtArea.lookup(".text").
                     getBoundsInLocal().getHeight()/txtArea.getParagraphLinesCount(0));*/
 
-
             if(txtField.getText().equals(toWrite.substring(0, txtField.getText().length()))){
                 Border b = new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
                 txtField.setBorder(b);
 
-                txtArea.displaceCaret(txtField.getText().length() + currentWord);
+                txtArea.setStyle(0, currentWord + txtField.getText().length(), "-fx-stroke: green;");
 
+                txtArea.displaceCaret(txtField.getText().length() + currentWord);
                /* if(currentWord > txtArea.getCurrentLineEndInParargraph()){
                     txtArea.scrollYBy(5);
                 }*/
@@ -95,24 +124,12 @@ public class Controller implements Initializable {
             }else{
                 Border b = new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT));
                 txtField.setBorder(b);
+
+                txtArea.setStyle(txtArea.getCaretPosition(), currentWord + txtField.getText().length(), "-fx-stroke: red;");
                 //txtArea.setStyle(1, 2, "");
                 //txtArea.getCaretBounds().get().getHeight();
 
             }
         }
     }
-
-   /* private double computeTextWidth(Font font, String text, double wrappingWidth) {
-        Text helper = new Text();
-        helper.setFont(font);
-        helper.setText(text);
-        // Note that the wrapping width needs to be set to zero before
-        // getting the text's real preferred width.
-        helper.setWrappingWidth(0);
-        helper.setLineSpacing(0);
-        double w = Math.min(helper.prefHeight(-1), wrappingWidth);
-        helper.setWrappingWidth((int)Math.ceil(w));
-        double textWidth = Math.ceil(helper.getLayoutBounds().getHeight());
-        return textWidth;
-    }*/
 }
