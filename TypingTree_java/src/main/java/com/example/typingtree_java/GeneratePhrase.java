1cc2f0dec3a1bf684cd60066a0f1b697562c13d8
StefanoManescotto <stefa.manescotto@gmail.com>
2022-07-17 22:25:57 +0200

    Added GeneratePhrase Class

---
+++ TypingTree_java/src/main/java/com/example/typingtree_java/GeneratePhrase.java
@@ -0,0 +1,43 @@
+package com.example.typingtree_java;
+
+import kotlin.jvm.Throws;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class GeneratePhrase {
+
+    /**
+     * @param wordsNum number of words to generate
+     * @return string of random num words
+     */
+    public static String getRandomWords(int wordsNum){
+        if(wordsNum <= 0){
+            return "";
+        }
+
+        String s = "";
+
+        for(int i = 0; i < wordsNum; i++){
+            s += getWord() + " ";
+        }
+
+        return s;
+    }
+
+    private static String getWord(){
+        try {
+            List<String> l = Files.readAllLines(Paths.get(GeneratePhrase.class.getResource("MostUsedWords").toURI()));
+            Random rand = new Random();
+            return l.get(rand.nextInt(l.size()));
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
